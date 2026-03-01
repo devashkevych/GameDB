91253cf638917344f0bbb304e0f12098d7c68f5c
TupacShampur <roma.ivashckevych@gmail.com>
2026-03-01 16:03:05 +0100

    GameDetails and GameCard error handling add

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,8 +1,10 @@
 export default function GameCard({ game }) {
+    const rating = game.rating
+
     return (
         <div>
             <h3>{game.name}</h3>
-            <p>{game.rating ? Number(game.rating).toFixed(1) : 'N/A'}</p>
+            <p>{rating !== null && rating !== undefined ? Number(game.rating).toFixed(1) : 'N/A'}</p>
             <img src={game.cover.url ? game.cover.url : 'No image'} alt="" />
         </div>
     )
